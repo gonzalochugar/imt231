12798c319fb62f09299f96739753db59f9ecee63
gonzalochugar <chugar90@gmail.com>
2024-09-24 09:07:44 -0400

    2024-09-24-01 controla ciclo hasta adivinar

--- juegoadivinanza.cpp
+++ juegoadivinanza.cpp
@@ -5,23 +5,34 @@ cout<<"***************************************"<<endl;
 cout<<"*Bienvenido al juego de la adivinanza!*"<<endl;
 cout<<"***************************************"<<endl;
 
-int numero_secreto = 42;
-cout << "el numero secreto es ..." << numero_secreto << ". No lo diga a nadie!" << endl;
-
+const int NUMERO_SECRETO = 42;
 int adivina;
-cout <<"¿Cual es el numero? ";
-cin >> adivina;
-cout << "El valor de su numero es: " << adivina << endl;
 
-if(adivina == numero_secreto){
+bool no_acerto = true;
+
+while (no_acerto){
+	cout << "cual es el numero?";
+	cin >> adivina;
+	cout << "el valor de su numero es: " << adivina << endl;
+
+	bool acerto = adivina == NUMERO_SECRETO;
+	bool mayor = adivina > NUMERO_SECRETO;
+
+if(acerto){
 	cout << "Felicitaciones!!! ... adivino el numero secreto" << endl;
+	no_acerto = false;
 	}
 
-else if(adivina > numero_secreto){
-	cout <<"El numero incresado es mayor que el numero secreto" << endl;
+else if(mayor){
+	cout << "El numero incresado es mayor que el numero secreto" << endl;
 	}
 
 else{
 	cout << "El numero incresado es menor que el numero secreto" <<endl;
 	}
+
+
+}
+cout << "Finalizo el juego" << endl;
+
 }
